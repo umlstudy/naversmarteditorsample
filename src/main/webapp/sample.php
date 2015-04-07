cbbbf14ea3180375964845a4d243cfb453cc2e51
sejong <umlstudy@gmail.com>
2015-04-07 15:54:51 +0900

    ignore

---
+++ src/main/webapp/sample.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ko" xml:lang="ko">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Smart Editor&#8482; WYSIWYG Mode</title>
+<link href="./css/compiled/open/ko_KR/smart_editor2_in.css" rel="stylesheet" type="text/css">
+</head>
+<body class="smartOutput se2_inputarea">
+	<p>
+		<b><u>에디터 내용:</u></b>
+	</p>
+
+	<div style="width:736px;">
+	<?php
+		$postMessage = $_POST["ir1"]; 
+		echo $postMessage;
+	?>
+	</div>
+</body>
+</html>
